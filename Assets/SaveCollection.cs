7dbfab45e087ae58596a893081b8e7d4cf262f6b
Garntachat <garntachatch@gmail.com>
2026-02-26 20:32:13 +0700

    Recruit + Collection system

--- Assets/SaveCollection.cs
+++ Assets/SaveCollection.cs
@@ -6,15 +6,42 @@ public class SaveCollection : MonoBehaviour
     public TMP_Text[] collectionList;
     void Start()
     {
-        
+
+        CollectionData data = SaveSystem.LoadCollection();
+
+        if (data != null)
+        {
+            for (int i = 0; i < collectionList.Length; i++)
+            {
+                collectionList[i].text = data.collectionStatus[i];
+            }
+        }
         
         string[] col = new string[collectionList.Length];
 
         for (int i = 0; i < collectionList.Length; i++)
         {
             col[i] = collectionList[i].text;
+            Debug.Log(col);
         }
+        
+        
+        SaveSystem.SaveCollection(col);
+        
 
+    }
+
+    public void resetCol()
+    {
+        string[] col = new string[collectionList.Length];
+
+        for (int i = 0; i < collectionList.Length; i++)
+        {
+            col[i] = "locked";
+            collectionList[i].text = "locked";
+        }
+        
+        
         SaveSystem.SaveCollection(col);
     }
 
